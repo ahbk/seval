736d437b4d8360e41dba4c45b503eda0aa8f6474
Alexander Holmbäck <alexande.holmback@creuna.se>
2018-09-15 15:13:50 +0000

    Add rotation functionality

--- public/script.js
+++ public/script.js
@@ -23,10 +23,33 @@ function draw(polygon, depth, context) {
   context.beginPath()
 
   polygon.forEach(v => {
+    // Rotate every vector 45 degrees in each dimension
+    v = rotate(v, [Math.PI/4, Math.PI/4, Math.PI/4])
     // Map 3d -> 2d
     context.lineTo(v[0] / (v[2] + depth) * depth, v[1] / (v[2] + depth) * depth)
   })
 
   context.closePath()
   context.stroke()
+}
+
+function transform(v, m) {
+  // Return a linear transformation of vector v by transformation matrix m
+  return [
+    m[0][0] * v[0] + m[0][1] * v[1] + m[0][2] * v[2],
+    m[1][0] * v[0] + m[1][1] * v[1] + m[1][2] * v[2],
+    m[2][0] * v[0] + m[2][1] * v[1] + m[2][2] * v[2],
+  ]
+}
+
+function rotate(v, r) {
+  // Return a rotation of vector v.
+  // Parameter r is an array with three elements giving the radians for the rotation (rx, ry, rz)
+  [
+    [ [1, 0, 0], [0, Math.cos(r[0]), -Math.sin(r[0])], [0, Math.sin(r[0]), Math.cos(r[0])], ],
+    [ [Math.cos(r[1]), 0, Math.sin(r[1])], [0, 1, 0], [-Math.sin(r[1]), 0, Math.cos(r[1])], ],
+    [ [Math.cos(r[2]), -Math.sin(r[2]), 0], [Math.sin(r[2]), Math.cos(r[2]), 0], [0, 0, 1], ]
+  ].forEach(m => { v = transform(v, m) })
+
+  return v
 }

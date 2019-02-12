a9b021fdd9ab8abbeb8a40a57735b73ef6d4b5cb
Alexander Holmbäck <alexander.holmback@gmail.com>
2019-02-12 15:21:44 +0000

    Separate package.json for client and server

--- tryout/tryout.js
+++ tryout/tryout.js
@@ -1,6 +1,6 @@
 ;(function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory()
-    : typeof define === 'function' && define.amd ? define(factory) : global.polygons = factory() // eslint-disable-line
+    : typeof define === 'function' && define.amd ? define(factory) : global.tryout = factory() // eslint-disable-line
 }(this, function () {
   'use strict'
 
